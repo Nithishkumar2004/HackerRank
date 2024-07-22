a3fac040e78b460c316e79e86277bf916b4be980
Nithishkumar S <nithishcse2021@gmail.com>
2024-07-22 21:24:13 +0530

    Solved 15th problem

---
+++ Easy Difficulty/Breaking the Records/Solution.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    least=high=0
+    min=max=scores[0]
+    for i in scores:
+        if min>i:
+            min=i
+            least+=1
+        if max<i:
+            max=i
+            high+=1
+    return [high,least]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
