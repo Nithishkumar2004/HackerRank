bec282474355e75dde83acd2e60e2a6df8c61df7
Nithishkumar S <nithishcse2021@gmail.com>
2024-07-22 19:57:52 +0530

    Solved 10th Problem in java&python

---
+++ birthday-cake-candles/Solution.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    # Find the height of the tallest candle
+    tallest = max(candles)
+    # Count how many candles have that height
+    count = candles.count(tallest)
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
