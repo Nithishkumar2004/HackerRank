e596218d5c8a1e747cb7c360c30bd228de072ddd
Nithishkumar S <nithishcse2021@gmail.com>
2024-07-23 20:33:09 +0530

    Solved 16th problem

---
+++ Easy Difficulty/Bill Division/Solution.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<Integer> bill, int k, int b) {
+    // Write your code here
+         // Calculate the total bill
+        int totalBill = 0;
+        for (int i = 0; i < bill.size(); i++) {
+            totalBill += bill.get(i);
+        }
+        
+        // Subtract the item Anna didn't eat
+        int annaShare = (totalBill - bill.get(k)) / 2;
+        
+        // Calculate the difference
+        int difference = b - annaShare;
+        
+        // Print the result
+        if (difference == 0) {
+            System.out.println("Bon Appetit");
+        } else {
+            System.out.println(difference);
+        }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
