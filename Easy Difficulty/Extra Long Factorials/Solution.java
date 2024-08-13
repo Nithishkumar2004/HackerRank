4c611fe574331f1c4d59e093cd34a1b11f44b0e7
NithishKumar S <nithishcse2021@gmail.com>
2024-08-13 10:10:20 +0530

    solved 40th problem in java

---
+++ Easy Difficulty/Extra Long Factorials/Solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+    // Write your code here
+    BigInteger factorial = BigInteger.ONE;
+    for(int i=2;i<=n;i++)
+    {
+        factorial = factorial.multiply(BigInteger.valueOf(i));
+    }
+    System.out.print(factorial);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
